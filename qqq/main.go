8e52225c99dc4d1b3f121eb8ad84134d3447b3ec
polosate <polosate@gmail.com>
2018-01-24 18:43:47 +0300

    ad two lists

--- qqq/main.go
+++ qqq/main.go
@@ -19,3 +19,37 @@ func main() {
 	}
 	fmt.Println(sum)
 }
+
+
+ type ListNode struct {
+   Val int
+   Next *ListNode
+ }
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var cur, first *ListNode
+	sum := 0
+	
+	for l1 != nil || l2 != nil || sum != 0 {
+		next := &ListNode{}
+		cur = next
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		if sum/10 > 0 {
+			cur.Val = sum%10
+			sum = sum/10
+		} else {
+			cur.Val = sum
+			sum = 0
+		}
+		cur = cur.Next
+	}
+
+	return first
+}
